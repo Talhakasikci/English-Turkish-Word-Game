a9038e04ce72b3e4fe60cdea3662d9c7479b0340
Talha Kasikci <talhakasikci377334@gmail.com>
2025-06-27 12:51:07 +0300

    some updates

---
+++ WordApp/app/src/main/java/com/talhakasikci/wordapp/ui/fragments/EntryPage.kt
@@ -0,0 +1,43 @@
+package com.talhakasikci.wordapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.talhakasikci.wordapp.R
+import com.talhakasikci.wordapp.databinding.FragmentEntryPageBinding
+
+
+class EntryPage : Fragment() {
+
+    private lateinit var binding: FragmentEntryPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentEntryPageBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.PractiseWordsCV.setOnClickListener {
+            val action = EntryPageDirections.actionEntryPageToMainFragment()
+            requireView().findNavController().navigate(action)
+        }
+    }
+
+
+}
